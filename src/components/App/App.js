46d190e84186fdb13ff7e3980dceca71d106412b
yuval datner <yuval@rbtree.io>
2019-07-18 05:32:58 +0300

    add page transition

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,39 @@
-import React from "react";
-import { ThemeProvider } from "styled-components";
+import React, { useState } from "react";
+import styled, { ThemeProvider } from "styled-components";
 import theme from "./theme";
 import Header from "../Header";
 import Info from "../Info";
+import Wheel from "../Wheel";
+import Container from "./Container";
+import { useEventListener } from "../../lib/hooks";
 
-const App = () => (
-  <ThemeProvider theme={theme}>
-    <div>
-      <Header />
-      <Info />
-    </div>
-  </ThemeProvider>
-);
+const BlockA = styled.div`
+  transform: ${props =>
+    props.toggle ? "translateY(-1000px)" : "translateY(0)"};
+  transition: ease-in-out 800ms;
+`;
+const BlockB = styled.div`
+  transform: ${props =>
+    props.toggle ? "translateY(-500px)" : "translateY(400px)"};
+  transition: ease-in-out 800ms;
+`;
+
+const App = () => {
+  const [toggle, setToggle] = useState(false);
+  useEventListener("keydown", e => e.key === "[" && setToggle(!toggle));
+  return (
+    <ThemeProvider theme={theme}>
+      <Container>
+        <BlockA toggle={toggle}>
+          <Header />
+          <Info />
+        </BlockA>
+        <BlockB toggle={toggle}>
+          <Wheel />
+        </BlockB>
+      </Container>
+    </ThemeProvider>
+  );
+};
 
 export default App;
---
+++ src/components/App/Container.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  overflow: hidden;
+  max-height: calc(100vh - 24px);
+`;
+
+export default Container;
